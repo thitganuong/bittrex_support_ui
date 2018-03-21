2830fed8efcdb356c445e6b76a56bc4e980000a0
Nguyen Thanh Hai <haint@co-well.jp>
2018-03-21 21:14:02 +0700

    no message

--- palmbeachpumptool.js
+++ palmbeachpumptool.js
@@ -7,12 +7,8 @@ $("head").append(s);
 function load() {
 	var context = document.querySelector(".contt");
 	var instance = new Mark(context);
-	//instance.mark("aion");
-	//["Lorem", "ipsum"])
 	instance.mark(["(aion)","(BTC)","(ETH)","(XMR)","(PPY)","(FCT)","(STORJ)","(ETC)","(DASH)","(XRP)","(OMG)","(ZEN)","(GTO)","(STEEM)","(NEO)","(CND)","(ICN)","(XEM)","(LSK)","(BAT)","(WAVES)","(MAID)","(EOS)","(BNB)","(XLM)","(DRGN)","(WAX)","(VEN)"]);
-	//$('#contt').highlight('aion');
 	addJumpButton();
-	
 };
 
 function addJumpButton(){
