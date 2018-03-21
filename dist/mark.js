99880fcbd5a9ac73aaa8cd502f8d3123fd9ea831
Nguyen Thanh Hai <haint@co-well.jp>
2018-03-21 13:24:30 +0700

    no message

--- dist/mark.js
+++ dist/mark.js
@@ -734,7 +734,7 @@
             startNode = node.splitText(start),
             ret = startNode.splitText(end - start);
         var repl = document.createElement(hEl);
-        repl.setAttribute('href', 'true');
+        repl.setAttribute('data-markjs', 'true');
         repl.setAttribute('href','https://www.google.co.jp');
         if (this.opt.className) {
           repl.setAttribute('class', this.opt.className);
