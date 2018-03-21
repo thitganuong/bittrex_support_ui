65a4fe941fd0d23dd756c23930fa2aa1cd224540
Nguyen Thanh Hai <haint@co-well.jp>
2018-03-21 19:43:11 +0700

    no message

--- palmbeachpumptool.js
+++ palmbeachpumptool.js
@@ -125,9 +125,22 @@ function load() {
 	//["Lorem", "ipsum"])
 	instance.mark(["(aion)","(BTC)","(ETH)","(XMR)","(PPY)","(FCT)","(STORJ)","(ETC)","(DASH)","(XRP)","(OMG)","(ZEN)","(GTO)","(STEEM)","(NEO)","(CND)","(ICN)","(XEM)","(LSK)","(BAT)","(WAVES)","(MAID)","(EOS)","(BNB)","(XLM)","(DRGN)","(WAX)","(VEN)"]);
 	//$('#contt').highlight('aion');
+	addJumpButton();
+	
+};
+
+function addJumpButton(){
+	var buttonLoad = document.createElement("button");
+	buttonLoad.innerHTML = "(.)(.)";
+	buttonLoad.className = "btn btn-default btn-toolbar";
+	buttonLoad.addEventListener ("click", jumpToFirstCoin);
+	document.getElementsByClassName('grid col-700')[0].appendChild(buttonLoad);
+}
+
+function jumpToFirstCoin(){
 	// jump to first coin 
 	document.getElementsByTagName('mark')[0].scrollIntoView();
-};
+}
 
 jQuery(window).load(function () {
 	load();
