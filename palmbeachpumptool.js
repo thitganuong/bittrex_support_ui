4c907ab52db74c3666e3a6a753f8c939539d1345
Nguyen Thanh Hai <haint@co-well.jp>
2018-03-21 12:39:47 +0700

    no message

--- palmbeachpumptool.js
+++ palmbeachpumptool.js
@@ -41,17 +41,27 @@ s.type = "text/javascript";
 s.src = "https://cdn.rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.es6.min.js";
 $("head").append(s);
 
-var buttonLoad = document.createElement("button");
-buttonLoad.innerHTML = "(.)(.)";
-buttonLoad.className = "btn btn-default btn-toolbar";
-$(buttonLoad).insertBefore($('#cursor-pointer').find('[type=button]'));
-buttonLoad.addEventListener ("click", load);
+//var buttonLoad = document.createElement("button");
+//buttonLoad.innerHTML = "(.)(.)";
+//buttonLoad.className = "btn btn-default btn-toolbar";
+//$(buttonLoad).insertBefore($('#cursor-pointer').find('[type=button]'));
+//buttonLoad.addEventListener ("click", load);
 
 function load() {
 	var context = document.querySelector(".contt");
 	var instance = new Mark(context);
 	instance.mark("aion",[ "mark"]);
 };
+
+jQuery(window).load(function () {
+	load();
+	  // alert('page is loaded');
+	   
+//	    setTimeout(function () {
+//	        alert('page is loaded and 1 minute has passed');   
+//	    }, 6); 
+});
+
 
 //getNonce();
 console.log("bittrex-api-ver1.js file is loaded");
