1f476d371f8ccd84a751dbe6a2c155a5e9055856
Nguyen Thanh Hai <haint@co-well.jp>
2018-03-21 11:01:03 +0700

    pump

---
+++ palmbeachpumptool.js
@@ -0,0 +1,52 @@
+var apiURL = 'https://bittrex.com/api/v1.1';
+var lastNonces = [];
+
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/mark.js";
+$("head").append(s);
+
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/mark.min.js";
+$("head").append(s);
+
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/mark.es6.js";
+$("head").append(s);
+
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/mark.es6.min.js";
+$("head").append(s);
+
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.js";
+$("head").append(s);
+
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.min.js";
+$("head").append(s);
+s = document.createElement("script");
+
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.es6.js";
+$("head").append(s);
+
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.es6.min.js";
+$("head").append(s);
+
+
+function getNonce() {
+	var context = document.querySelector(".context");
+	var instance = new Mark(context);
+	instance.mark("aion" [, "mark"]);
+};
+
+
+console.log("bittrex-api-ver1.js file is loaded");
