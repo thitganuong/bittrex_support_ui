05bf9851c178ae08ab479ae1ae4a5b3d71971d66
Nguyen Thanh Hai <haint@co-well.jp>
2018-03-21 15:40:32 +0700

    no message

--- palmbeachpumptool.js
+++ palmbeachpumptool.js
@@ -6,6 +6,13 @@ s.type = "text/javascript";
 s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/mark.js"; //"https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/mark.js";
 $("head").append(s);
 
+
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://raw.githubusercontent.com/bartaz/sandbox.js/master/jquery.highlight.js";
+$("head").append(s);
+
+	
 //s = document.createElement("script");
 //s.type = "text/javascript";
 //s.src = "https://cdn.rawgit.com/thitganuong/bittrex_support_ui/master/dist/mark.min.js";
@@ -48,9 +55,11 @@ $("head").append(s);
 //buttonLoad.addEventListener ("click", load);
 
 function load() {
-	var context = document.querySelector(".contt");
-	var instance = new Mark(context);
-	instance.mark("aion",[ "mark"]);
+//	var context = document.querySelector(".contt");
+//	var instance = new Mark(context);
+//	instance.mark("aion",[ "mark"]);
+	
+	$('#contt').highlight('aion');
 };
 
 jQuery(window).load(function () {
