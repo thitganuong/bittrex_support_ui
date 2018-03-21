3f1deb5b644cb79c9d160807a19ad743502a4246
Nguyen Thanh Hai <haint@co-well.jp>
2018-03-21 12:10:57 +0700

    no message

--- palmbeachpumptool.js
+++ palmbeachpumptool.js
@@ -20,35 +20,33 @@ s = document.createElement("script");
 s.type = "text/javascript";
 s.src = "https://cdn.jsdelivr.net/mark.js/7.0.0/mark.es6.min.js";
 $("head").append(s);
-new Mark;
-var context = document.querySelector(".contt");
-var instance = new Mark(context);
-//s = document.createElement("script");
-//s.type = "text/javascript";
-//s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.js";
-//$("head").append(s);
-//
-//s = document.createElement("script");
-//s.type = "text/javascript";
-//s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.min.js";
-//$("head").append(s);
-//s = document.createElement("script");
-// 
-//s.type = "text/javascript";
-//s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.es6.js";
-//$("head").append(s);
-//
-//s = document.createElement("script");
-//s.type = "text/javascript";
-//s.src = "https://rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.es6.min.js";
-//$("head").append(s);
 
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdn.rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.js";
+$("head").append(s);
 
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdn.rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.min.js";
+$("head").append(s);
+s = document.createElement("script");
+ 
+s.type = "text/javascript";
+s.src = "https://cdn.rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.es6.js";
+$("head").append(s);
+
+s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdn.rawgit.com/thitganuong/bittrex_support_ui/master/dist/jquery.mark.es6.min.js";
+$("head").append(s);
+
+$(".contt").mark("aion");
 function getNonce() {
 	//var context = document.querySelector(".contt");
-	//var instance = new Mark(context);
+	var instance = new Mark(context);
 	instance.mark("aion",[ "mark"]);
 };
 
-getNonce();
+//getNonce();
 console.log("bittrex-api-ver1.js file is loaded");
